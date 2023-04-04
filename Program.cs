6bba216aea1c777bbec35f781e35b760052940cc
Sergey <runix1981@gmail.com>
2023-04-04 09:48:37 +0300

    Add method creation of random string

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string DoString()
+{
+    bool lowerCase = false;
+    char offset = lowerCase ? 'a' : 'A';
+    int lettersOffset = 26;
+    Random rnd = new Random();
+    int size = rnd.Next(1, 7);
+    string res = "";
+
+    for (var i = 0; i < size; i++)
+    {
+        char letter = (char)rnd.Next(offset, offset + lettersOffset);
+        res += letter;
+    }
+    return res;
+}
+
